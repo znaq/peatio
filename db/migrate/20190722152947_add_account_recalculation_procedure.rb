8bd68ac7fe8cca568a1497a76a1e92280a4b730c
Ali Shanaakh <github@shal.dev>
2019-07-24 16:08:18 +0300

    Reimplement account recalculation

--- db/migrate/20190722152947_add_account_recalculation_procedure.rb
+++ db/migrate/20190722152947_add_account_recalculation_procedure.rb
@@ -24,8 +24,7 @@ class AddAccountRecalculationProcedure < ActiveRecord::Migration[5.2]
 
         -- Save and compact liabilities.
         CREATE TEMPORARY TABLE squashed_liabilities AS
-        SELECT NULL, code, currency_id, member_id, 'Job', job_id, SUM(debit) as debit, SUM(credit) AS credit, NOW() AS created_at, NOW() AS updated_at
-
+        SELECT code, currency_id, member_id, 'Job', job_id, SUM(debit) AS debit, SUM(credit) AS credit
         FROM liabilities
         GROUP BY code, currency_id, member_id;
 
@@ -37,42 +36,33 @@ class AddAccountRecalculationProcedure < ActiveRecord::Migration[5.2]
         -- Squash the liabilities.
         TRUNCATE liabilities;
         INSERT INTO liabilities
-        SELECT * FROM squashed_liabilities;
+        SELECT NULL, code, currency_id, member_id, 'Job', job_id, SUM(debit) AS debit, SUM(credit) AS credit, NOW() AS created_at, NOW() AS updated_at
+        FROM squashed_liabilities
+        GROUP BY code, currency_id, member_id;
 
         -- Drop the temporaty table.
         DROP TEMPORARY TABLE IF EXISTS squashed_liabilities;
 
-        -- Calculate balance of accounts.
-        CREATE TEMPORARY TABLE accounts_main
-        SELECT ac.member_id, ac.currency_id, SUM(lb.credit) - SUM(lb.debit) AS amount
-        FROM accounts as ac JOIN liabilities as lb
-        USING(member_id, currency_id) WHERE lb.code IN
-        (
+        CREATE TEMPORARY TABLE tmp_accounts
+        SELECT ac.id AS id, IFNULL(main.credit - main.debit, 0) AS balance, IFNULL(locked.credit - locked.debit, 0) AS locked
+        FROM accounts AS ac
+        LEFT JOIN liabilities as main ON ac.member_id = main.member_id && ac.currency_id = main.currency_id && main.code IN (
           SELECT code
           FROM operations_accounts
           WHERE `type` = 'liability' AND kind = 'main'
         )
-        GROUP BY lb.code, lb.currency_id, lb.member_id;
-
-        -- Calculate locked of accounts.
-        CREATE TEMPORARY TABLE accounts_locked
-        SELECT ac.member_id, ac.currency_id, SUM(lb.credit) - SUM(lb.debit) AS amount
-        FROM accounts as ac JOIN liabilities as lb
-        USING(member_id, currency_id) WHERE lb.code IN
-        (
+        LEFT JOIN liabilities as locked ON ac.member_id = locked.member_id && ac.currency_id = locked.currency_id && locked.code IN (
           SELECT code
           FROM operations_accounts
           WHERE `type` = 'liability' AND kind = 'locked'
-        )
-        GROUP BY lb.code, lb.currency_id, lb.member_id;
+        );
 
         -- Update accounts.
         UPDATE accounts
-          INNER JOIN accounts_main USING(member_id, currency_id)
-          INNER JOIN accounts_locked USING(member_id, currency_id)
+        INNER JOIN tmp_accounts ON tmp_accounts.id = accounts.id
         SET
-          accounts.balance = accounts_main.amount,
-          accounts.locked = accounts_locked.amount,
+          accounts.balance = tmp_accounts.balance,
+          accounts.locked = tmp_accounts.locked,
           accounts.updated_at = NOW();
 
         -- Set job state and rows affected.
@@ -82,8 +72,7 @@ class AddAccountRecalculationProcedure < ActiveRecord::Migration[5.2]
           UPDATE jobs SET `rows` = ROW_COUNT(), `state` = 'failed' WHERE id = job_id;
         END IF;
 
-        DROP TEMPORARY TABLE IF EXISTS accounts_main;
-        DROP TEMPORARY TABLE IF EXISTS accounts_locked;
+        DROP TEMPORARY TABLE IF EXISTS tmp_accounts;
       END
     SQL
   end
